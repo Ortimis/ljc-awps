7656f35304515858748575a8bbfaa613daec21dd
Ortimis <jakob@drortmann.de>
2018-06-19 11:45:00 +0200

    Blog Shortcode

--- functions.php
+++ functions.php
@@ -30,4 +30,60 @@ function nachlese_excerpt() {
     }
     return apply_filters('the_excerpt', $text);
 }
+
+add_shortcode( 'foerderverein-loop', 'get_foerderverein_loop');
+
+function get_foerderverein_loop() {
+    // WP_Query arguments
+    $args = array(
+        'post_type'              => array('post'),
+        'post_status'            => 'publish',
+        'posts_per_page' 		 => 10,
+        'category_name'          => 'förderverein'
+    );
+
+    // The Query
+    $query_aktuelles = new WP_Query( $args );
+
+
+
+    // The Loop
+    if ( $query_aktuelles->have_posts() ) {
+    ?>
+
+    <section id="aktuelles-jubilaum" class="alignfull">
+
+        <div class="container">
+
+                    
+                    <div id="primary" class="content-area">
+                        <main id="aktuelles-wrapper" class="site-main" role="main">
+                            <?php
+
+                                while ( $query_aktuelles->have_posts() ) {
+                                    $query_aktuelles->the_post();
+
+                                    get_template_part('views/content');
+
+                                }
+                            ?>
+
+                        </main><!-- #main -->
+                    </div><!-- #primary -->
+
+                    
+                    <a href="<?php echo home_url(); ?>/blog" class="btn btn-sm btn-light btn-transp mt-10 mb-5">Zum Blog</a>
+        </div><!-- .container -->
+
+    </section>
+
+    <?php
+
+    } else {
+        echo '<pre>Nothing Found</pre>';
+    }
+
+    // Restore original Post Data
+    wp_reset_postdata();
+}
 		
