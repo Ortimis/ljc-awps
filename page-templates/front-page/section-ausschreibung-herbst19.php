4cfbaf3c104677db1004e4b62190b27ea6841830
Ortimis <jakob@drortmann.de>
2019-02-20 17:01:13 +0100

    textänderungen ausschreibung herbst 19

--- page-templates/front-page/section-ausschreibung-herbst19.php
+++ page-templates/front-page/section-ausschreibung-herbst19.php
@@ -13,12 +13,16 @@
 			<article id="post-<?php the_ID(); ?>" <?php post_class('phase-front container'); ?> >
 				<div class="row justify-center">
 					<div class="col-lg-5 col-md-6 call-to-action call-to-action--left">
-						<h1 class="phase-titel">Herbst 2019</h1>
+						<h1 class="phase-titel">Herbstphase 2019</h1>
 					</div>
 					<div class="col-lg-4 col-md-6">
 						<h2>Ausschreibung</h2>
 						<p>
-							Die Anmeldung zur Herbstphase 2019 erfolgt nur postalisch. Das ausgefüllte Anmeldeformular bitte unterschrieben einsenden. 
+						Aus Anlass unseres 40jährigen Chorjubiläums hat sich unser Förderverein mächtig ins Zeug gelegt und es geschafft, dem LJC ein chorsinfonisches Projekt ersten Ranges zu ermöglichen: Mit 120 Sängerinnen und Sängern können wir in den Herbstferien das Verdi-Requiem erarbeiten. <b>Auch ehemalige LJC-Mitglieder sind herzlich eingeladen mitzumachen.</b> <br>
+						Die Anmeldung erfolgt ausschließlich postalisch mit verbindlicher Unterschrift! <br><br>
+						Wir können nur Personen aufnehmen, die an der gesamten Arbeits- und Konzertphase vollumfänglich teilnehmen. Durch die Bewerbung besteht noch kein Anspruch auf Teilnahme.
+						Wir freuen uns auf Eure Anmeldungen!
+
 						</p>
 						<div class="btn-group mt-5 mb-10">
 							<a target="_blank" href="<?php echo home_url();?>/wp-content/uploads/2019/02/Ausschreibung-Herbst-2019.pdf" class="btn btn-sm btn-light">Anmeldeformular</a>
