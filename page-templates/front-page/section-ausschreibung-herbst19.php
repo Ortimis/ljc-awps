4ff851589285ef87645456cb8bc227ef0b29c2aa
Ortimis <jakob@drortmann.de>
2019-03-02 11:46:49 +0100

    Text changed Ausschreibung herbst 19

--- page-templates/front-page/section-ausschreibung-herbst19.php
+++ page-templates/front-page/section-ausschreibung-herbst19.php
@@ -20,7 +20,8 @@
 						<p>
 						Aus Anlass unseres 40jährigen Chorjubiläums hat sich unser Förderverein mächtig ins Zeug gelegt und es geschafft, dem LJC ein chorsinfonisches Projekt ersten Ranges zu ermöglichen: Mit 120 Sängerinnen und Sängern können wir in den Herbstferien das Verdi-Requiem erarbeiten. <b>Auch ehemalige LJC-Mitglieder sind herzlich eingeladen mitzumachen.</b> <br>
 						Die Anmeldung erfolgt ausschließlich postalisch mit verbindlicher Unterschrift! <br><br>
-						Wir können nur Personen aufnehmen, die an der gesamten Arbeits- und Konzertphase vollumfänglich teilnehmen. Durch die Bewerbung besteht noch kein Anspruch auf Teilnahme.
+						Wir können nur Personen aufnehmen, die an der gesamten Arbeits- und Konzertphase vollumfänglich teilnehmen. Durch die Bewerbung besteht noch kein Anspruch auf Teilnahme.<br><br>
+						Wir bitten um Verständnis dafür, dass wir aus organisatorischen Gründen auf unserer Jubiläumsphase keine neuen Interessenten einladen können, sondern nur Bestandsmitglieder und Ehemalige.<br><br>
 						Wir freuen uns auf Eure Anmeldungen!
 
 						</p>
